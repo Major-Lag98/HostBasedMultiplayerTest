b97441006058068a35eae9b5ca5c25bf10213404
Alden <aldensmith06@Gmail.com>
2020-07-14 14:32:11 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerConnectionObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerConnectionObject : NetworkBehaviour
+{
+    public GameObject PlayerUnitPrefab;
+
+    GameObject myPlayerUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer == false)
+        {
+            //this obj belongs to another player
+            return;
+        }
+
+
+        Debug.Log("Spawning personal unit...");
+        //Instantiate(PlayerUnitPrefab);
+        CmdSpawnMyUnit();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.V)) // V to spawn unit
+        {
+            CmdSpawnMyUnit();
+        }
+        if (Input.GetKeyDown(KeyCode.X)) // X to destroy unit
+        {
+            CmdDestroyMyUnit();
+        }
+    }
+
+    [Command]
+    void CmdSpawnMyUnit() // create a unit, cache it, give athority to client and let everyone else know it exists.
+    {
+        if (myPlayerUnit != null) //player should only spawn and controll one thing
+        {
+            return;
+        }
+        GameObject player = Instantiate(PlayerUnitPrefab);
+        myPlayerUnit = player;
+        NetworkServer.Spawn(player);
+        player.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient); //spawn first then set athority or else it complains
+        
+    }
+
+    [Command]
+    void CmdDestroyMyUnit()
+    {
+        NetworkServer.Destroy(myPlayerUnit);
+    }
+}
---
+++ Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerUnit : NetworkBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update() // no need to write code to let people know where obj location is because networkTransform componenet handles it already
+    {
+        if (hasAuthority == false)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(1, 0, 0);
+        }
+    }
+}
